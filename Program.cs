8c71abb8e30666390e5e80737c5415143d4dcad1
ömer can <omer.can34@outlook.com>
2021-02-07 00:25:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kelime;
+            string cevir;
+            Console.Write("kelime giriniz (q çıkış):");
+            kelime = Console.ReadLine();
+            while (kelime != "q")
+            {
+                
+                cevir = ceviri(kelime);
+                if (kelime == cevir)
+                {
+                    Console.WriteLine("Girdiğiniz kelime palindromdur.");
+                }
+                else
+                {
+                    Console.WriteLine("Girdiğiniz kelime palindrom değildir.");
+                }
+                Console.Write("kelime giriniz (q çıkış):");
+                kelime = Console.ReadLine();
+               
+            }
+
+
+
+            
+
+
+        }
+        static string ceviri(string kelime)
+        {
+
+            char[] metindizi = kelime.ToCharArray();                // char tipinde bir array tanımladık girilen kelimeyi harflerine ayırdık
+            Array.Reverse(metindizi);                              // metin dizisini tersten sıraladık
+            string donenmetin = new string(metindizi);            //metin dizisini stringe çevirdik yeni bir değişkene atadık
+            return donenmetin;                                   //atadığımız değişkeni fonksiyonun çıktsı
+
+        }
+    }
+}
